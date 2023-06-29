a2afa10c7bc7524d9f93bc586c332e03aa7481c4
Bikram Shrestha <stha.bikram999@gmail.com>
2023-06-29 18:54:54 +0545

    All completed

---
+++ Authorization/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Authorization.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Authorization.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<SalesLeadEntity> SalesLead { get; set; }
+        public DbSet<ApplicationUser> ApplicationUser { get; set; }
+    }
+}
---
+++ Authorization/Models/SalesLeadEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace Authorization.Models
+{
+    public class SalesLeadEntity
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+    }
+}
